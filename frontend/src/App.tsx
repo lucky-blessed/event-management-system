9e87da6e7705b7d93852aeec5ce20020188208f7
lucky-blessed <blessednkwor7@gmail.com>
2024-10-18 14:11:39 +0100

    Setting up antD for UI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { Button, Input } from "antd";
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-500">
-      <h1 className="text-white text-3xl font-bold">Homepage</h1>
+    <div className="h-screen flex justify-center items-center flex-col gap-10 p-10">
+      <h1 className="text-gray-500 text-3xl font-bold">Homepage</h1>
+
+      <Button type="primary">Primary Button</Button>
+
+      <Button>Default Button</Button>
+
+      <Input placeholder="For simple use"></Input>
     </div>
   );
 }
 
-export default App
+export default App;
